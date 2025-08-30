6913cdb5856678bed49f8976d34b094bf397a015
Anas Ali Abd Elmoneim <anas.dev000@gmail.com>
2025-08-30 02:30:45 +0200

    fix more errors and edit payment and ...

--- Models/productModel.js
+++ Models/productModel.js
@@ -1,48 +1,50 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
     },
     description: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 500,
-        index: "text"
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 500,
+      index: "text",
     },
     price: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     quantity: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     categoryId: {
-        type: mongoose.Types.ObjectId,
-        ref: "Category",
-        required: true
-    },  
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
     images: {
-        type: [String],
-        required: true,
+      type: [String],
+      required: true,
     },
     addedBy: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true
-    }
-}, { 
-    versionKey: false, 
-    timestamps: true
-});
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const ProductModel = mongoose.model("Product", productSchema);
 export default ProductModel;
-
